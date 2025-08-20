de0ddde5d57ac003d322ea87e073003ea656e507
anveshavyas <anvesha.vyas23@vit.edu>
2025-08-20 11:08:28 +0530

    Create main.jsx

---
+++ frontend/main.jsx
@@ -0,0 +1,12 @@
+import "leaflet/dist/leaflet.css";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
